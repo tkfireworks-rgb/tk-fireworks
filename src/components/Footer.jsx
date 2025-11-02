0ac0f96f1781a3d330c280f66df9d39f3aac776e
Magushiru <vjandsjchannel@gmail.com>
2025-11-02 21:03:00 +0530

    Add html.css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 TK Fireworks. All sparks reserved. 🔥</p>
+    </footer>
+  );
+}
---
+++ src/components/LanguageSwitcher.jsx
@@ -0,0 +1,16 @@
+import { useTranslation } from 'react-i18next';
+
+export default function LanguageSwitcher() {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div>
+      <button onClick={() => changeLanguage('en')}>English</button>
+      <button onClick={() => changeLanguage('ta')}>தமிழ்</button>
+    </div>
+  );
+}
